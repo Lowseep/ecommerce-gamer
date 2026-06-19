f11433362bd93b0eaa0067ec6daab9074a43ce83
Lowseep <pierosamaniego60@gmail.com>
2026-06-18 19:26:51 -0500

    Ajustes de configuracion para URLs correctas en contenedores Docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,24 +4,24 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         if (config('app.url')) {
             URL::forceRootUrl(config('app.url'));
+            URL::forceScheme(parse_url(config('app.url'), PHP_URL_SCHEME));
         }
+
+        Paginator::currentPathResolver(function () {
+            return url()->current();
+        });
     }
 }
